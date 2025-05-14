58b01e2aada46174e42c5414c930b53ee5d7a864
Nairi-Khachatryan <nairi.khachatryan357@gmail.com>
2025-05-14 19:11:26 +0400

    Added login and registration page, downgraded React from v19 to v18, and set user in Redux after registration

---
+++ src/pages/signIn/SignIn.tsx
@@ -0,0 +1,115 @@
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../services/firebse-config';
+import { Button, Checkbox, Form, Input } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import type { FormProps } from 'antd';
+import { useState } from 'react';
+import './SignIn.css';
+import { SIGN_UP } from '../../routes/paths';
+
+type FieldType = {
+  email: string;
+  password: string;
+  remember: boolean;
+};
+
+const SignIn = () => {
+  const [error, setError] = useState<string>('');
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+
+  const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+    const { email, password } = values;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        form.setFieldsValue({ password: '' });
+      } else {
+        setError('An unknown error occurred');
+        form.setFieldsValue({ password: '' });
+      }
+    }
+  };
+
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (
+    errorInfo
+  ) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div className="sign-in-container">
+      <Form
+        className="form"
+        form={form}
+        name="sign-in"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        style={{
+          width: '100vw',
+          maxWidth: 550,
+          minWidth: 280,
+          boxSizing: 'border-box',
+          height: 340,
+        }}
+      >
+        <Form.Item<FieldType>
+          label="Email"
+          name="email"
+          rules={[
+            {
+              type: 'email',
+              required: true,
+              message: 'Please input your Email!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="password"
+          label="Password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <div>
+          <h3 className="error" style={{ color: 'red' }}>
+            {error}
+          </h3>
+        </div>
+
+        <div className="signup-redirect">
+          <span>Don't have an account?</span>
+          <Button type="link" onClick={() => navigate(SIGN_UP)}>
+            Sign Up
+          </Button>
+        </div>
+
+        <div className="submit-remember-container">
+          <Form.Item<FieldType> name="remember" valuePropName="checked">
+            <Checkbox className="remember">Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Sign In
+            </Button>
+          </Form.Item>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,21 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "../pages/Home"
-import About from "../pages/About"
-import Login from "../pages/Login"
-import NotFound from "../pages/NotFound"
+import { ABOUT, SIGN_IN, SIGN_UP, HOME, NOT_FOUND_PAGE } from './paths';
+import { Routes, Route } from 'react-router-dom';
+import SignUp from '../pages/signUp/SignUp';
+import SignIn from '../pages/signIn/SignIn';
+import NotFound from '../pages/NotFound';
+import About from '../pages/About';
+import Home from '../pages/Home';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path={HOME} element={<Home />} />
+      <Route path={ABOUT} element={<About />} />
+      <Route path={SIGN_IN} element={<SignIn />}></Route>
+      <Route path={SIGN_UP} element={<SignUp />}></Route>
+      <Route path={NOT_FOUND_PAGE} element={<NotFound />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
