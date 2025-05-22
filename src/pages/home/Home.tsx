26667518b4ab26ab9f6be4f034eedb85ea339577
mellaff <143594199+mellaff@users.noreply.github.com>
2025-05-22 10:57:34 +0400

    antd-skeleton-home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import { Layout, Input, Button, Select, Card, Carousel } from "antd";
 import { useTranslation } from "react-i18next";
 import { useTheme } from "../../contexts/ThemeContext";
 import "./Home.css";
+import WorkerCardSkeleton from "../../components/skeletons/WorkerCardSkeleton.tsx";
 
 const { Content } = Layout;
 const { Option } = Select;
@@ -11,9 +12,18 @@ const Home: React.FC = () => {
   const carouselRef = useRef<any>(null);
   const { t } = useTranslation();
   const { theme } = useTheme();
+  const [loading, setLoading] = React.useState(true);
 
   const next = () => carouselRef.current?.next();
   const prev = () => carouselRef.current?.prev();
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <Layout className={`homepage homepage-${theme}`}>
@@ -42,17 +52,21 @@ const Home: React.FC = () => {
             ←
           </Button>
           <Carousel ref={carouselRef} autoplay className="worker-carousel">
-            {Array.from({ length: 7 }).map((_, index) => (
-              <div key={index} className="carousel-slide">
-                <Card className="worker-card" variant="outlined">
-                  <p className="worker-rate">★★★★★</p>
-                  <div className="worker-image" />
-                  <p className="worker-name">{t("worker.name")}</p>
-                  <p className="worker-surname">{t("worker.surname")}</p>
-                  <p className="worker-profession">{t("worker.profession")}</p>
-                </Card>
-              </div>
-            ))}
+            {loading
+                ? Array.from({ length: 4 }).map((_, index) => (
+                    <WorkerCardSkeleton key={index} />
+                ))
+                : Array.from({ length: 7 }).map((_, index) => (
+                    <div key={index} className="carousel-slide">
+                      <Card className="worker-card" variant="outlined">
+                        <p className="worker-rate">★★★★★</p>
+                        <div className="worker-image" />
+                        <p className="worker-name">{t("worker.name")}</p>
+                        <p className="worker-surname">{t("worker.surname")}</p>
+                        <p className="worker-profession">{t("worker.profession")}</p>
+                      </Card>
+                    </div>
+                ))}
           </Carousel>
           <Button onClick={next} className="carousel-btn">
             →
