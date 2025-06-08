0348bcd589b9a92b121f36482309be66b39be53b
Nairi-Khachatryan <nairi.khachatryan357@gmail.com>
2025-06-08 19:11:51 +0400

    Enhanced resume form layout — skills and languages now appear on separate lines for better readability.

--- src/utils/resumeFormat.ts
+++ src/utils/resumeFormat.ts
@@ -1,9 +1,10 @@
 export function formatResumePrompt(values: any): string {
+  console.log(values, 'values')
   return `
 You are a resume generator.
 
 Take the following user information and generate a resume in **valid JSON format** only. 
-Do not include any extra text or Markdown. Structure it with these fields:
+Do not include any extra text or Markdown. Structure it with these fields, skills and languages is important parth.
 
 
 - contactInfo: { email, phone, city, name, lastName }
@@ -12,6 +13,10 @@ Do not include any extra text or Markdown. Structure it with these fields:
 - education
 - skills: array
 - languages: array
+
+dont forgot find and add language and skills
+
+
 
 Here is the user input:
 ${JSON.stringify(values, null, 2)}
