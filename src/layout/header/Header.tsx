5a96ac2738e19116917b6992c86fd349723f13dd
Tigran <tigran.nazaryan.18@gmail.com>
2025-06-11 07:15:37 -0700

    add burger menu

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,20 +1,19 @@
-import { LANGUAGE_STORAGE_KEY } from '../../constants/storageKeys';
-import { StorageService } from '../../services/StorageService';
-import { Button, Col, Modal, Row, Select, Space } from 'antd';
-import { MoonFilled, SunOutlined } from '@ant-design/icons';
-import logoWhite from '../../assets/images/logo-white.png';
-import logoDark from '../../assets/images/logo-dark.png';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useTheme } from '../../contexts/ThemeContext';
-import hyFlag from '../../assets/images/flags/am.png';
-import usFlag from '../../assets/images/flags/us.png';
-import ruFlag from '../../assets/images/flags/ru.png';
-import { useAuth } from '../../contexts/AuthContext';
-import { useTranslation } from 'react-i18next';
-import { useEffect, useState } from 'react';
-import { ROUTES } from '../../routes/paths';
-import './header.css';
-
+import { LANGUAGE_STORAGE_KEY } from "../../constants/storageKeys";
+import { StorageService } from "../../services/StorageService";
+import { Button, Col, Drawer, Modal, Row, Select, Space } from "antd";
+import { MenuOutlined, MoonFilled, SunOutlined } from "@ant-design/icons";
+import logoWhite from "../../assets/images/logo-white.png";
+import logoDark from "../../assets/images/logo-dark.png";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useTheme } from "../../contexts/ThemeContext";
+import hyFlag from "../../assets/images/flags/am.png";
+import usFlag from "../../assets/images/flags/us.png";
+import ruFlag from "../../assets/images/flags/ru.png";
+import { useAuth } from "../../contexts/AuthContext";
+import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import { ROUTES } from "../../routes/paths";
+import "./header.css";
 
 const flags = {
   en: usFlag,
@@ -26,19 +25,19 @@ const Header = () => {
   const navigate = useNavigate();
   const { theme, handleClick } = useTheme();
   const { isAuth, logout } = useAuth();
-  const [language, setLanguage] = useState<'en' | 'hy' | 'ru'>('en');
+  const [language, setLanguage] = useState<"en" | "hy" | "ru">("en");
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
     const savedLang = StorageService.getItem(LANGUAGE_STORAGE_KEY) as
-      | 'en'
-      | 'hy'
-      | 'ru'
+      | "en"
+      | "hy"
+      | "ru"
       | null;
     if (savedLang) setLanguage(savedLang);
   }, []);
 
-  const onLanguageChange = (lang: 'en' | 'hy' | 'ru') => {
+  const onLanguageChange = (lang: "en" | "hy" | "ru") => {
     setLanguage(lang);
     i18n.changeLanguage(lang);
     StorageService.setItem(LANGUAGE_STORAGE_KEY, lang);
@@ -46,10 +45,10 @@ const Header = () => {
 
   const handleLogout = () => {
     Modal.confirm({
-      title: t('confirmLogoutTitle'),
-      content: t('confirmLogoutContent'),
-      okText: t('yes'),
-      cancelText: t('no'),
+      title: t("confirmLogoutTitle"),
+      content: t("confirmLogoutContent"),
+      okText: t("yes"),
+      cancelText: t("no"),
       onOk: async () => {
         logout();
         navigate(ROUTES.HOME_PATH);
@@ -57,16 +56,20 @@ const Header = () => {
       okButtonProps: { danger: true },
     });
   };
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => setOpen(true);
+  const closeDrawer = () => setOpen(false);
 
   const AuthButtons = () => (
     <Space.Compact className="auth-buttons">
       {isAuth ? (
         <>
           <Button onClick={() => navigate(ROUTES.PROFILE_PATH)}>
-            {t('Profile')}
+            {t("Profile")}
           </Button>
           <Button type="primary" danger onClick={handleLogout}>
-            {t('Logout')}
+            {t("Logout")}
           </Button>
         </>
       ) : (
@@ -75,13 +78,13 @@ const Header = () => {
             className="sign-in-btn"
             onClick={() => navigate(ROUTES.SIGN_IN_PATH)}
           >
-            {t('signIn')}
+            {t("signIn")}
           </Button>
           <Button
             className="sign-up-btn"
             onClick={() => navigate(ROUTES.SIGN_UP_PATH)}
           >
-            {t('signUp')}
+            {t("signUp")}
           </Button>
         </>
       )}
@@ -102,7 +105,7 @@ const Header = () => {
               <div className="logo" onClick={() => navigate(ROUTES.HOME_PATH)}>
                 <img
                   className="logo-image"
-                  src={theme === 'dark' ? logoDark : logoWhite}
+                  src={theme === "dark" ? logoDark : logoWhite}
                   alt="logo"
                 />
               </div>
@@ -112,24 +115,65 @@ const Header = () => {
               <nav className="nav-container">
                 <ul className="list-container">
                   <li>
-                    <NavLink to={ROUTES.HOME_PATH}>{t('home')}</NavLink>
+                    <NavLink to={ROUTES.HOME_PATH}>{t("home")}</NavLink>
                   </li>
                   <li>
-                    <NavLink to={ROUTES.ABOUT_PATH}>{t('about')}</NavLink>
+                    <NavLink to={ROUTES.ABOUT_PATH}>{t("about")}</NavLink>
                   </li>
                   <li>
                     <NavLink to={ROUTES.CONTACT_US_PATH}>
-                      {t('contact')}
+                      {t("contact")}
                     </NavLink>
                   </li>
                   <li>
-                    <NavLink to={ROUTES.JOBS_PATH}>{t('Jobs')}</NavLink>
+                    <NavLink to={ROUTES.JOBS_PATH}>{t("Jobs")}</NavLink>
                   </li>
                 </ul>
               </nav>
             </Col>
           </Row>
         </Col>
+
+        <>
+          {/* Бургер-кнопка только на мобильных экранах */}
+          <Button
+            type="text"
+            icon={<MenuOutlined />}
+            onClick={showDrawer}
+            className="mobile-burger-button"
+          />
+
+          {/* Drawer из Ant Design */}
+          <Drawer
+            placement="left"
+            onClose={closeDrawer}
+            open={open}
+            title="Меню"
+          >
+            <ul className="list-container">
+              <li>
+                <NavLink to={ROUTES.HOME_PATH} onClick={closeDrawer}>
+                  {t("home")}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={ROUTES.ABOUT_PATH} onClick={closeDrawer}>
+                  {t("about")}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={ROUTES.CONTACT_US_PATH} onClick={closeDrawer}>
+                  {t("contact")}
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={ROUTES.JOBS_PATH} onClick={closeDrawer}>
+                  {t("Jobs")}
+                </NavLink>
+              </li>
+            </ul>
+          </Drawer>
+        </>
 
         <Col>
           <Row align="middle" gutter={16} className="right-section">
@@ -141,7 +185,7 @@ const Header = () => {
                 popupMatchSelectWidth={false}
                 optionLabelProp="label"
               >
-                {(['en', 'hy', 'ru'] as const).map((lang) => (
+                {(["en", "hy", "ru"] as const).map((lang) => (
                   <Select.Option
                     key={lang}
                     value={lang}
@@ -152,21 +196,21 @@ const Header = () => {
                           alt={lang}
                           className="flag-icon"
                         />
-                        {lang === 'en'
-                          ? 'English'
-                          : lang === 'hy'
-                          ? 'Հայերեն'
-                          : 'Русский'}
+                        {lang === "en"
+                          ? "English"
+                          : lang === "hy"
+                          ? "Հայերեն"
+                          : "Русский"}
                       </div>
                     }
                   >
                     <div className="option-item">
                       <img src={flags[lang]} alt={lang} className="flag-icon" />
-                      {lang === 'en'
-                        ? 'English'
-                        : lang === 'hy'
-                        ? 'Հայերեն'
-                        : 'Русский'}
+                      {lang === "en"
+                        ? "English"
+                        : lang === "hy"
+                        ? "Հայերեն"
+                        : "Русский"}
                     </div>
                   </Select.Option>
                 ))}
@@ -175,7 +219,7 @@ const Header = () => {
 
             <Col>
               <div className="theme-toggle" onClick={handleClick}>
-                {theme === 'light' ? (
+                {theme === "light" ? (
                   <MoonFilled className="theme-icon" />
                 ) : (
                   <SunOutlined className="theme-icon" />
